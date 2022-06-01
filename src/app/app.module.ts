8ed96924a3cae0753e16795cc4bde7c13917ca54
Mirko Marković <mirko.markovic@logate.com>
2022-06-01 15:18:04 +0200

    Finish main layout and topbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TopbarComponent } from './topbar/topbar.component';
+import { PageContentComponent } from './page-content/page-content.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, TopbarComponent, PageContentComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
